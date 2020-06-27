c62d9d26c6c7ab523fe626e7be9bb484433bccd6
Chamod Shehanka <hcsperera@outlook.com>
2020-06-27 19:44:27 +0530

    Nav bar making

--- src/app/components/common/nav/nav.component.ts
+++ src/app/components/common/nav/nav.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  isLoggedIn: Boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.isLoggedIn = false;
   }
 
+  ngOnInit(): void {}
 }
