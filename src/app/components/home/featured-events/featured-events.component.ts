e2fe0e86307f33efc18dd3c8417879eeb2cbe099
Chamod Shehanka <hcsperera@outlook.com>
2020-06-27 15:55:12 +0530

    Featured Events Componenent Added

---
+++ src/app/components/home/featured-events/featured-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-events',
+  templateUrl: './featured-events.component.html',
+  styleUrls: ['./featured-events.component.css']
+})
+export class FeaturedEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
