470f32cbc8272689e788610f65d0943036fd00a9
Chamod Shehanka <hcsperera@outlook.com>
2020-06-28 15:43:38 +0530

    Event Model Added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,50 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../../models/event.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {
+    this.tempEvents = [
+      new Event(
+        'GoLang Sri Lanka - June',
+        'Mon, Jun 29, 4:50 PM',
+        'https://secure-content.meetupstatic.com/images/classic-events/491000108/375x210.jpg',
+        'GoLang Sri Lanka Community',
+        12,
+        'Online'
+      ),
+      new Event(
+        'Power BI Community: #002',
+        'Tue, Jun 30, 5:00 PM',
+        'https://secure-content.meetupstatic.com/images/classic-events/491024724/500x280.jpg',
+        'Sri Lankan Power BI Community',
+        30,
+        'Online'
+      ),
 
-  constructor() { }
-
-  dummyData = [
-    {
-      dateTime: 'Mon, Jun 29, 4:50 PM',
-      title: 'GoLang Sri Lanka - June',
-      image: 'https://secure-content.meetupstatic.com/images/classic-events/491000108/375x210.jpg',
-      organizer: 'GoLang Sri Lanka Community',
-      participants: 12,
-      type: 'Online'
-    },
-    {
-      dateTime: 'Tue, Jun 30, 5:00 PM',
-      title: 'Power BI Community: #002',
-      image: 'https://secure-content.meetupstatic.com/images/classic-events/491024724/500x280.jpg',
-      organizer: 'Sri Lankan Power BI Community',
-      participants: 30,
-      type: 'Online'
-    },
-    {
-      dateTime: 'Tue, Jun 30, 5:00 PM',
-      title: 'Flutter Layout`s Design Secrets',
-      image: 'https://secure-content.meetupstatic.com/images/classic-events/490906389/500x280.jpg',
-      organizer: 'Colombo Flutter Community',
-      participants: 19,
-      type: 'Online'
-    },
-    {
-      dateTime: 'Tue, Jun 30, 5:00 PM',
-      title: 'Scaffolding for a React Application',
-      image: 'https://secure-content.meetupstatic.com/images/classic-events/490705895/500x280.jpg',
-      organizer: 'Colombo React Native Meetup Group',
-      participants: 39,
-      type: 'Online'
-    }
-  ];
-
-  ngOnInit(): void {
+      new Event(
+        'Flutter Layout`s Design Secrets',
+        'Tue, Jun 30, 5:00 PM',
+        'https://secure-content.meetupstatic.com/images/classic-events/490906389/500x280.jpg',
+        'Colombo Flutter Community',
+        19,
+        'Online'
+      ),
+      new Event(
+        'Scaffolding for a React Application',
+        'Tue, Jun 30, 5:00 PM',
+        'https://secure-content.meetupstatic.com/images/classic-events/490705895/500x280.jpg',
+        'Colombo React Native Meetup Group',
+        39,
+        'Online'
+      ),
+    ];
   }
 
+  tempEvents: Event[];
+
+  ngOnInit(): void {}
 }
