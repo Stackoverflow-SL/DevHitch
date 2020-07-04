8cf17ff00da4b1f2c24bc056df485f513f0333ec
Chamod Shehanka <hcsperera@outlook.com>
2020-07-04 19:28:18 +0530

    New Group Details Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NewGroupComponent } from './components/new-group/new-group.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'new-group', component: NewGroupComponent },
 ];
 
 @NgModule({
--- src/app/components/new-group/new-group.component.ts
+++ src/app/components/new-group/new-group.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Topic } from '../../models/topic.model';
 
 @Component({
   selector: 'app-new-group',
   templateUrl: './new-group.component.html',
-  styleUrls: ['./new-group.component.css']
+  styleUrls: ['./new-group.component.css'],
 })
 export class NewGroupComponent implements OnInit {
+  topics: Topic[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.topics = [
+      new Topic('1', 'Computer Science'),
+      new Topic('2', 'DevOps'),
+      new Topic('3', 'Software Engineering'),
+      new Topic('4', 'Beginner'),
+      new Topic('5', 'IT'),
+      new Topic('6', 'Computer Science')
+    ];
   }
 
+  ngOnInit(): void {}
 }
